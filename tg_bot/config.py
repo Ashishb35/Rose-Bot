f9d664399bf181b3f422d04ce367e3931f847d76
Ashish Bagh <104833737+Ashishb35@users.noreply.github.com>
2022-05-13 15:32:34 +0530

    Update and rename sample_config.py to config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "5120192788:AAEphFPsLzEgzMxYcKIhhk_uVfMZ6KrcBlY"
+    OWNER_ID = "641846423"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "Ashish"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
