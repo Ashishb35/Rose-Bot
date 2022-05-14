984970ca05bbd7ff95f6fc423209fcebe037f9ba
Ashish Bagh <104833737+Ashishb35@users.noreply.github.com>
2022-05-14 15:16:03 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "5394611321:AAF8zn32sI6vmLDp_s46mkvCnhm5teTfFZ0"
-    OWNER_ID = "641846423"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "Ashish"
+    API_KEY = "YOUR KEY HERE"
+    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "YOUR USERNAME HERE"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
@@ -23,7 +23,7 @@ class Config(object):
     URL = None
 
     # OPTIONAL
-    SUDO_USERS = []  # List of id's (631486423) for users which have sudo access to the bot.
+    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
     SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = None  # EG, paypal
